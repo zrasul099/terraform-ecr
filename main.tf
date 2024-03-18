4e080808d83c465e080b3ca19e39f53e773fb28e
zrasul099 <zrasul099@gmail.com>
2024-03-18 18:14:49 -0500

    ecr

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws"{
-  region =     "us-west-2"
+  region = "us-west-2"
 }
 resource "aws_ecr_repository" "foo" {
   name                 = "bar"
-           image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
-    scan_on_push =             true
+    scan_on_push = true
   }
 }
 terraform {
